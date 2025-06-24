44f48f97c261b907b683167786380c6ae9363aeb
jung mo <gromit25@hanmail.net>
2025-06-24 15:43:03 +0900

    크론잡 기능 추가

--- GenDBSpec/src/main/java/com/redeye/dbspec/GenDBSpecApplication.java
+++ GenDBSpec/src/main/java/com/redeye/dbspec/GenDBSpecApplication.java
@@ -15,6 +15,14 @@ import com.redeye.dbspec.exporter.SchemaExporter;
  */
 @SpringBootApplication
 public class GenDBSpecApplication implements CommandLineRunner {
+
+	/** 실행 모드 */
+	@Value("${app.run-mode}")
+	private RunMode runMode;
+
+	/** 크론 실행 모드일 경우의 실행 주기 */
+	@Value("${app.cron.schedule}"}
+	private Stirng cronSchedule;
 	
 	/**
 	 * 출력 객체
@@ -39,7 +47,25 @@ public class GenDBSpecApplication implements CommandLineRunner {
 	 * @param args 명령행 인자
 	 */
 	@Override
-    public void run(String... args) throws Exception {
-		this.exporter.export();
-    }
+	public void run(String... args) throws Exception {
+		
+		if(this.runMode == RunMode.CRON) {
+
+			// 크론잡 실행
+			CronJob.builder()
+				.cronExp(this.cronSchedule)
+				.job(new Runnable() {
+					@Override
+					public void run() {
+						this.exporter.export();
+					}
+				})
+				.build().run();
+			
+		} else {
+			
+			// 한번만 실행
+			this.exporter.export();
+		}
+	}
 }
