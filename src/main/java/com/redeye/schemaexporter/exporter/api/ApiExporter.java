d8c575524f69dbf68ed3602d9e46c5666d193f41
gromit25 <gromit25@hanmail.net>
2025-09-06 13:20:06 +0900

    API 용 JSON 생성 추가

--- src/main/java/com/redeye/schemaexporter/exporter/api/ApiExporter.java
+++ src/main/java/com/redeye/schemaexporter/exporter/api/ApiExporter.java
@@ -1,16 +1,23 @@
 package com.redeye.schemaexporter.exporter.api;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Map;
 
-import com.jutools.PublishUtil;
+import com.jutools.publish.Publisher;
+import com.jutools.publish.PublisherFactory;
+import com.jutools.publish.PublisherType;
 import com.redeye.schemaexporter.exporter.Exporter;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 
  * 
  * @author jmsohn
  */
+@Slf4j
 public class ApiExporter extends Exporter {
 	
 	/** 포맷 파일 명 */
@@ -23,7 +30,15 @@ public class ApiExporter extends Exporter {
 	    // 출력 format input stream
 		InputStream formatInputStream = getClass().getClassLoader().getResourceAsStream(FORMAT_FILE);
 		
-	    // 엑셀 파일 출력 실행
-	    PublishUtil.toTxt(null, null, null, values);;
+		// JSON 출력 실행
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		Publisher publisher = PublisherFactory.create(PublisherType.TEXT_FILE, formatInputStream);
+		publisher.publish(out, Charset.forName("UTF-8"), values);
+		
+		String schemaJSON = new String(out.toByteArray(), "UTF-8");
+		log.info("SCHEMA JSON: " + schemaJSON);
+		
+		// API 호출
 	}
 }
